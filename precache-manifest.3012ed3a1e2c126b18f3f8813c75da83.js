e5d2829a396c558f14b20602868c4b9c92addf16
Vigyan <vigyan.kayastha@gmail.com>
2020-06-22 13:41:58 -0400

    Updates

---
+++ precache-manifest.3012ed3a1e2c126b18f3f8813c75da83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5ca7cdc80950b926cd4dc4ddf92a27",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "33626b39d5fca33ab91e",
+    "url": "/weather-react-app/static/css/main.895ce0d9.chunk.css"
+  },
+  {
+    "revision": "3813f49663736df6921a",
+    "url": "/weather-react-app/static/js/2.0c263891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.0c263891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33626b39d5fca33ab91e",
+    "url": "/weather-react-app/static/js/main.46ea690a.chunk.js"
+  },
+  {
+    "revision": "80a11080e1d5d2b66f46",
+    "url": "/weather-react-app/static/js/runtime-main.42a25d67.js"
+  }
+]);
