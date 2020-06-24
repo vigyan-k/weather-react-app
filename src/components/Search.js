79c541859d36d0cebb1c9ece8ca6f71c818b8a22
Vigyan <vigyan.kayastha@gmail.com>
2020-06-23 23:52:43 -0400

    media queries

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,6 +10,7 @@ class Search extends Component{
             <h1>Weather <span>Radar</span>  <FontAwesomeIcon icon={faSearchLocation} className='searchIcon'/></h1>
             <IconsRow />
             <form action="" onSubmit={this.props.handleSubmit}>
+              <label htmlFor="search" className='visuallyHidden'></label>
               <input onChange={this.props.handleChange}type="text" placeholder='City' />
             </form>
             <button onClick={this.props.handleClick}>Get the Weather!</button>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown, faBolt, faCloudRain, faCloudShowersHeavy, faSnowflake, faSun, faCloud, faSmog } from '@fortawesome/free-solid-svg-icons';
 
-const Weather = ({cityName, country, currentTemp, feelsLike, minTemp, maxTemp, weatherDescription}) => {
+const Weather = ({cityName, country, currentTemp, feelsLike, minTemp, maxTemp, weatherDescription, icon, main}) => {
+    // const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    let weatherIcon = undefined;
+
+    if (main === 'Clear') {
+        weatherIcon = <FontAwesomeIcon icon={faSun} />;
+    }  else if (main === 'Clouds') {
+        weatherIcon = <FontAwesomeIcon icon={faCloud} />;
+    } else if (main === 'Rain') {
+        weatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} />;
+    }   else if (main === 'Drizzle') {
+        weatherIcon = <FontAwesomeIcon icon={faCloudRain} />;
+    } else if (main === 'Snow') {
+        weatherIcon = <FontAwesomeIcon icon={faSnowflake} />;
+    } else if (main === 'Thunderstorm') {
+        weatherIcon = <FontAwesomeIcon icon={faBolt} />;
+    } else {
+        weatherIcon = <FontAwesomeIcon icon={faSmog} />;
+    }
+    
     return (
         <div className='modal' id='modal'>
             <div className='row1'>
@@ -8,18 +29,19 @@ const Weather = ({cityName, country, currentTemp, feelsLike, minTemp, maxTemp, w
                     <p>{cityName}, {country}</p>
                 </div>
                 <div className='square squareTwo'>
-                    <p>Right now, it is {currentTemp}</p>
-                    <p>Feels like {feelsLike}</p>
+                    <p>{currentTemp}{'\u00b0'}</p>
+                    <p>Feels like {feelsLike}{'\u00b0'}</p>
                 </div>
             </div>
 
             <div className="row2">
                 <div className='square squareThree'>
+                    <p className='weatherIcon'>{weatherIcon}</p>
                     <p className='desc'>{weatherDescription}</p>
                 </div>
                 <div className='square squareFour'>
-                    <p>Low: {minTemp}</p>
-                    <p>High: {maxTemp}</p>
+                    <p><FontAwesomeIcon icon={faChevronDown}className='chevron'/>{minTemp}{'\u00b0'}</p>
+                    <p><FontAwesomeIcon icon={faChevronUp} className='chevron'/> {maxTemp}{'\u00b0'}</p>
                 </div>
             </div>
         </div>
